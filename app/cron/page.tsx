24945aadacb8f598226c9bffd1bf06b9211401c2
Mohammed Ahad <kulahad007@gmail.com>
2024-09-18 16:24:39 +1200

    Update cron method

--- app/cron/page.tsx
+++ app/cron/page.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 async function page() {
-  const data = await fetch("https://tldr-fijinews-api.onrender.com/grabnews");
-  const trigger: string = await data.json();
+  const data = await fetch("https://tldr-fijinews-api.onrender.com/grabnews", {
+    method: "Post",
+  });
+  const trigger: { message: string } = await data.json();
+  console.log(trigger);
 
-  return <div>{trigger}</div>;
+  return (
+    <div className="flex m-6 w-screen h-screen justify-center items-center text-3xl font-extrabold text-red-400">
+      Scrapping triggered! - {trigger.message}
+    </div>
+  );
 }
 
 export default page;
